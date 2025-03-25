9fff71a30c0c3ff04d8fc5afeab45c5b1d66edcf
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-03-25 13:00:43 +0530

    practice

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -197,7 +197,7 @@ function Home() {
             <section id='whystoryNoi' className="bg-[url('./assets/whystoryNoi-bg.png')]  bg-no-repeat bg-cover bg-center sm:py-[120px] py-[40px] ">
                 <div className='container flex flex-wrap xl:flex-nowrap sm:gap-[80px] gap-[30px] items-center justify-center'>
                     <div className='xl:w-1/2 order-2 xl:order-1 flex gap-[47px]' >
-                        <img src={whystory1} alt="" className='w-[100%] h-auto' />
+                        <img src={whystory1} alt="" className='w-[100%] h-auto mb-[90px]' />
                         <img src={whystory2} alt=""  className='self-end w-[100%] h-auto mt-[90px]'/>
 
                     </div>
