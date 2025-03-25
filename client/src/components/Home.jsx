575bfb1fcf2f39fcd89870137c1e66e43b402978
shubhamdhatwalia1@gmail.com <shubhamdhatwalia1@gmail.com>
2025-03-25 12:46:54 +0530

    practice

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -173,7 +173,7 @@ function Home() {
 
             <section id="storyNoi" className="bg-[url('./assets/storyNoibg.png')] bg-no-repeat bg-cover bg-center sm:py-[120px] py-[40px] relative">
 
-                <div className="container flex flex-wrap lg:flex-nowrap">
+                <div className="container flex flex-wrap lg:flex-nowrap relative">
                     <div className='lg:w-1/2 '>
                         <h2 data-aos="fade-right">All One Needs Is An Idea!</h2>
                         <h1 data-aos="fade-right">AI Personalized Stories</h1>
@@ -230,7 +230,7 @@ function Home() {
                     </div>
 
                     <div className="lg:w-1/2 w-full justify-items-center">
-                        <img src={ideaimg} alt="" className='m-[-30px]' />
+                        <img src={ideaimg} alt="" className='m-[-30px]' data-aos="fade-left" />
                     </div>
                 </div>
             </section>
